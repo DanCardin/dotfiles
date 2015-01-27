2cc426a1b925a69fadbbe4cb84adcc32d7fb8634
Dan Cardin <dcardin@insightsquared.com>
2015-01-27 21:28:21 +0000

    Renamed .* to *

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
-ln -s ~/.dotfiles/.bashrc ~/.bashrc
-ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/bash_aliases ~/.bash_aliases
 
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 
 ln -s ~/.dotfiles/.vim ~/.vim
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/vimrc ~/.vimrc
 
-ln -s ~/.dotfiles/.zprezto ~/.zprezto
-ln -s ~/.dotfiles/.zprezto/runcoms/zshrc ~/.zshrc
-ln -s ~/.dotfiles/.zprezto/runcoms/zpreztorc ~/.zpreztorc
-ln -s ~/.dotfiles/.zprezto/runcoms/zlogin ~/.zlogin
-ln -s ~/.dotfiles/.zprezto/runcoms/zlogout ~/.zlogout
-ln -s ~/.dotfiles/.zprezto/runcoms/zprofile ~/.zprofile
-ln -s ~/.dotfiles/.zprezto/runcoms/zshenv ~/.zshenv
+ln -s ~/.dotfiles/zprezto ~/.zprezto
+ln -s ~/.dotfiles/zprezto/runcoms/zshrc ~/.zshrc
+ln -s ~/.dotfiles/zprezto/runcoms/zpreztorc ~/.zpreztorc
+ln -s ~/.dotfiles/zprezto/runcoms/zlogin ~/.zlogin
+ln -s ~/.dotfiles/zprezto/runcoms/zlogout ~/.zlogout
+ln -s ~/.dotfiles/zprezto/runcoms/zprofile ~/.zprofile
+ln -s ~/.dotfiles/zprezto/runcoms/zshenv ~/.zshenv
 
