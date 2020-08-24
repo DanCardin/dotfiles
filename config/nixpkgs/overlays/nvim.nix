d62290f4baa260baac2c8b92a329a9d5ce0d029c
Dan Cardin <ddcardin@gmail.com>
2020-08-24 10:37:46 -0400

    update nvim overlay.

--- config/nixpkgs/overlays/nvim.nix
+++ config/nixpkgs/overlays/nvim.nix
@@ -4,8 +4,8 @@ self: super: {
     src = super.fetchFromGitHub {
       owner = "neovim";
       repo = "neovim";
-      rev = "94b7ff730a1914c14f347f5dc75175dc34a4b3f5";
-      sha256 = "15fpihn2xbdzp4nb1sgni0wyr94q89y45jaxfmzh6vjbx8f76m0w";
+      rev = "8c49e3d50959e24dadd688f56a18f104bd5fd934";
+      sha256 = "1qqmfys8hm60j4hgw861yad2x94jadd7247jjgqsxl4ij123hgdg";
     };
     buildInputs = old.buildInputs ++ [ super.utf8proc ];
   });
